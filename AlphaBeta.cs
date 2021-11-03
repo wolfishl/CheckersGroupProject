3ef4c83fb60fe3b4d28c540f8c99a9760052dbf7
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-11-02 21:28:02 -0400

    more changes to make alpha beta compatible with checkers, also began a rough blueprint for the board class

--- AlphaBeta.cs
+++ AlphaBeta.cs
@@ -8,5 +8,80 @@ namespace CheckersGame
 {
     class AlphaBeta
     {
+
+        public static int NrEntries = 0;
+        public static double Value(Board board, int depth, double alfa, double beta, Player player)
+        {
+            Trace.println("Enter alphabeta d = " + depth + " a = " + alfa + " b = " + beta + " P = " + player, 5);
+            ++NrEntries;
+            double value = 0.0;
+            if (depth == 0)
+            {
+                value = board.heuristicValue();
+            }
+            //change this for checkers
+            else if (board.isEmpty())
+            {
+                value = board.heuristicValue();
+            }
+            else
+            {
+                Player opponent = player == Player.MAX ? Player.MIN : Player.MAX;
+
+                if (player == Player.MAX)
+                {
+                    for (int col = 0; col < Board.SIZE; ++col)
+                    {
+                        for (int row = 0; row < Board.SIZE; ++row)
+                        {
+                            if (board.canMove(Player.MAX, col, row))
+                            {
+                                foreach (Board nextPos in board.MakeMove(Player.MAX, col, row))
+                                { 
+                                    double thisVal = Value(nextPos, depth - 1, alfa, beta, opponent);
+                                    if (thisVal > alfa)
+                                    {
+                                        alfa = thisVal;
+                                    }
+                                    if (beta <= alfa)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    value = alfa;
+                }
+                else  // player == Player.MIN
+                {
+                    for (int col = 0; col < Board.SIZE; ++col)
+                    {
+                        for (int row = 0; row < Board.SIZE; ++row)
+                        {
+                            if (board.canMove(Player.MIN, col, row))
+                            {
+                                foreach (Board nextPos in board.MakeMove(Player.MIN, col, row))
+                                {
+                                    double thisVal = Value(nextPos, depth - 1, alfa, beta, opponent);
+                                    if (thisVal < beta)
+                                    {
+                                        beta = thisVal;
+                                    }
+                                    if (beta <= alfa)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    value = beta;
+                }
+            }
+            Trace.println("Exit alfabeta value = " + value + " depth " + depth, 5);
+            return value;
+        }
+
     }
 }
--- Board.cs
+++ Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace CheckersGame
@@ -6,12 +7,35 @@ namespace CheckersGame
     public class Board
     {
         Button [,] board;
+        public static readonly int SIZE = 8;
 
         public Board(Button [,] board)
         {
             this.board = board;
         }
         internal double heuristicValue()
+        {
+            throw new NotImplementedException();
+        }
+       
+        internal bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         * returns a boolean that is true if the specified player can move on that piece
+         * note: must check that there is a piece in that square
+         */
+        internal bool canMove(Player player, int col, int row)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         * returns a list of possible moves for the piece specified in that column
+         */
+        internal List<Board> MakeMove(Player player, int col, int row)
         {
             throw new NotImplementedException();
         }
