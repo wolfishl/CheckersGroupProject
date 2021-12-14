84ae6fa49b56858444c9c4d4ecb924a5d874ac97
lwolfish <58049823+lwolfish@users.noreply.github.com>
2021-12-14 09:44:27 -0500

    Added player and location classes

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame
+{
+    /*
+     * White is MAX, Gray is MIN
+     */
+    public enum Player
+    {
+        MAX, MIN
+    }
+}
